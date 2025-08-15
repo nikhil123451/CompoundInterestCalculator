4996486adb71042d8728ee226a5316616f2d72d9
nikti <nikti@Andy>
2025-08-15 11:29:01 -0600

    renamed variables to fit java coding conventions

--- CompoundInterestCalculator/src/CompoundInterestCalculator.java
+++ CompoundInterestCalculator/src/CompoundInterestCalculator.java
@@ -5,29 +5,29 @@ public class CompoundInterestCalculator {
 	
 	public static void main(String[] args) {
 		System.out.print("Initial Amount: ");
-		double ini = scn.nextDouble();
-		System.out.print("Annual Interest Rate: ");
-		double intRate = scn.nextDouble();
+		double initialInterestAmount = scn.nextDouble();
+		System.out.print("Annual Interest Rate (as a decimal): ");
+		double interestRate = scn.nextDouble();
 		System.out.print("Contributions? (Y/N): ");
-		String contr = scn.next();
+		String contributionStatus = scn.next();
 		
-		double Mcontr = 0;
-		boolean isContr = contr.toUpperCase().contains("Y");
-		if (isContr) {
+		double monthlyContribution = 0;
+		boolean isContributing = contributionStatus.toUpperCase().contains("Y");
+		if (isContributing) {
 			System.out.print("Monthly Contributions: ");
-			Mcontr = scn.nextDouble();
+			monthlyContribution = scn.nextDouble();
 		}
 		
 		System.out.print("Years Saved: ");
 		int years = scn.nextInt();
 		
-		double balance = ini;
+		double balance = initialInterestAmount;
 		
 		for (int year = 1 ; year <= years ; year++) {
 			for (int month = 1 ; month <= 12 ; month++) { //12 months in a year
-				balance += Mcontr;
+				balance += monthlyContribution;
 				if (month == 12) { //end of the year
-					balance *= 1 + intRate;
+					balance *= 1 + interestRate;
 				}
 			}
 		}
